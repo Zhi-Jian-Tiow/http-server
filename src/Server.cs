2b4ac00e6bcb0897f9cf03a8302c6bbf2dca35be
Zhi Jian Tiow <aaron.tiow.99@gmail.com>
2024-05-21 23:02:00 +0100

    Extract URL Path - attept 1

--- src/Server.cs
+++ src/Server.cs
@@ -9,4 +9,13 @@ Console.WriteLine("Logs from your program will appear here!");
 TcpListener server = new TcpListener(IPAddress.Any, 4221);
 server.Start();
 var socket = server.AcceptSocket(); // wait for client
-socket.Send(Encoding.UTF8.GetBytes("HTTP/1.1 200 OK\r\n\r\n"));
+
+var requestBuffer = new byte[1024];
+var receivedBytes = socket.Receive(requestBuffer);
+
+var request = Encoding.UTF8.GetString(requestBuffer);
+var httpRequestLine = request.Split("\r\n")[0];
+var (httpMethod, targetUrl, httpVersion) = (httpRequestLine.Split(" ")[0], httpRequestLine.Split(" ")[1], httpRequestLine.Split(" ")[2]);
+
+var response = targetUrl == "/" ? $"{httpVersion} 200 OK\r\n\r\n" : $"{httpVersion} 404 NOT FOUND\r\n\r\n";
+socket.Send(Encoding.UTF8.GetBytes(response));
